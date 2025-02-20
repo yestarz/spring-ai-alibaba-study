c76bb33ad9f779cfa5ceaeb8e8a13f1fab6f7ac7
yangxin <981987024@qq.com>
2025-02-20 10:45:12 +0800

    EmbeddingModel的基本使用

--- 4-embedding-model/src/main/java/cn/baruto/ai/study/embeddingmodel/TestController.java
+++ 4-embedding-model/src/main/java/cn/baruto/ai/study/embeddingmodel/TestController.java
@@ -1,14 +1,48 @@
 package cn.baruto.ai.study.embeddingmodel;
 
 
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.embedding.DashScopeEmbeddingOptions;
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.embedding.EmbeddingRequest;
+import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
     @Resource
     private ChatClient chatClient;
+
+    @Resource
+    private EmbeddingModel embeddingModel;
+
+    @GetMapping("/ai/embedding")
+    public Map embed(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        EmbeddingResponse embeddingResponse = this.embeddingModel.embedForResponse(List.of(message));
+        return Map.of("embedding", embeddingResponse);
+    }
+
+    /**
+     * 指定嵌入模型
+     * @param message
+     * @return
+     */
+    @GetMapping("/ai/embedding2")
+    public Map embed2(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        EmbeddingResponse embeddingResponse = embeddingModel.call(
+                new EmbeddingRequest(List.of("Hello World", message),
+                        DashScopeEmbeddingOptions.builder()
+                                .withModel("text-embedding-v1")
+                                .build()));
+        return Map.of("embedding", embeddingResponse);
+    }
 
 }
---
+++ 5-function/src/main/java/cn/baruto/ai/study/function/FunctionApplication.java
@@ -0,0 +1,11 @@
+package cn.baruto.ai.study.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionApplication.class, args);
+    }
+}
