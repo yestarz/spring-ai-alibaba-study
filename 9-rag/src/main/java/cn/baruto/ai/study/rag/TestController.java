ba991b50cea8bc387fb7418a7a02bcfe5ecc7eb4
yangxin <981987024@qq.com>
2025-02-21 11:09:38 +0800

    RAG

--- 9-rag/src/main/java/cn/baruto/ai/study/rag/TestController.java
+++ 9-rag/src/main/java/cn/baruto/ai/study/rag/TestController.java
@@ -3,6 +3,10 @@ package cn.baruto.ai.study.rag;
 
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +15,16 @@ public class TestController {
     @Resource
     private ChatClient chatClient;
 
+    @Resource
+    private VectorStore vectorStore;
+
+
+    @GetMapping(value = "/chat")
+    public ChatResponse generation(String userInput) {
+        // 发起聊天请求并处理响应
+        return chatClient.prompt()
+                .user(userInput)
+                .advisors(new QuestionAnswerAdvisor(vectorStore))
+                .call().chatResponse();
+    }
 }
--- 9-rag/src/main/java/cn/baruto/ai/study/rag/configuration/ChatClientConfiguration.java
+++ 9-rag/src/main/java/cn/baruto/ai/study/rag/configuration/ChatClientConfiguration.java
@@ -7,8 +7,14 @@ import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class ChatClientConfiguration {
@@ -16,7 +22,7 @@ public class ChatClientConfiguration {
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
         return builder
-                .defaultSystem("你是一个乐于助人的智能聊天助手，请根据用户的问题给出简洁明了的回答。")
+                .defaultSystem("你将作为一名机器人产品的转接，对于y用户的使用需求做出解答")
 
                 // Advisor：顾问的意思
                 // 实现ChatMemory的Advisor
@@ -37,5 +43,23 @@ public class ChatClientConfiguration {
 
     }
 
+    @Bean
+    public VectorStore vectorStore(EmbeddingModel embeddingModel) {
+        SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(embeddingModel).build();
+
+        // 生成一个机器人产品说明书的文档
+
+        List<Document> documents = List.of(
+                new Document("产品说明书:产品名称：智能机器人\n" +
+                        "产品描述：智能机器人是一个智能设备，能够自动完成各种任务。\n" +
+                        "功能：\n" +
+                        "1. 自动导航：机器人能够自动导航到指定位置。\n" +
+                        "2. 自动抓取：机器人能够自动抓取物品。\n" +
+                        "3. 自动放置：机器人能够自动放置物品。\n" +
+                        "4. 自动写代码：机器人能够自动编写代码。\n" )
+        );
+        simpleVectorStore.add(documents);
+        return simpleVectorStore;
+    }
 
 }
